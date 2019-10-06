12ab56f84b30e0d5611ea98b1e753dd51acfce8b
Clarson1229 <27305718+clarson1229@users.noreply.github.com>
2019-10-06 13:00:08 -0500

    code commented

--- Lab 1/Source Code /WordCount.java
+++ Lab 1/Source Code /WordCount.java
@@ -15,18 +15,21 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
-
+    // Mapper class is below
     public static class FriendMapper
             extends Mapper<Object, Text, Text, Text> {
+                // creates our variables. 
         private final static IntWritable one = new IntWritable(1);
         private Text user = new Text();
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
+            // splits up the data based on parameters and stores it into arrays
             String[] linedata = value.toString().split("->");
             String friend1 = linedata[0];
             String[] friendlist = linedata[1].split(",");
             for (int i = 0; i< friendlist.length; i++ ){
+                // compares the order of the pairs and writes the key value pair. 
                 String friend2 = friendlist[i];
                 if (friend1.charAt(0) < friend2.charAt(0)) {
                     user.set(friend1 + "," + friend2);
@@ -38,15 +41,19 @@ public class WordCount {
 
         }
     }
-
+    // Reducer class is below
     public static class FriendReducer
             extends Reducer<Text, Text, Text, Text> {
+                // variable creation 
         private Text result = new Text();
 
         public void reduce(Text key, Iterable<Text> values,
                            Context context
         ) throws IOException, InterruptedException {
             Set<String> set1 = new HashSet<String>();
+            // loops through the values. 
+            // If the set is empty it adds it to the set
+            // If not it does a .retainAll() to do set intersection to only keep the ones that are in both sets. 
             for (Text friends: values){
                  String[] temp =friends.toString().split(",");
                 if (set1.isEmpty()){
@@ -57,6 +64,7 @@ public class WordCount {
                     set1.retainAll(set2);
                 }
             }
+            // converts the Set to a string and writes the result to the output file.
             Object[] joinedList= set1.toArray();
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i< joinedList.length; i++){
@@ -66,7 +74,7 @@ public class WordCount {
             context.write(key,result);
         }
     }
-
+    // main class that assigns jobs and runs our above functions. 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
